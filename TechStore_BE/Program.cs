8c113e127358cfdc423e7c4977d75746609b0acf
MinNghiax <tonghia2004pm@gmail.com>
2025-11-18 22:29:16 +0700

    Add backend source code

---
+++ TechStore_BE/Program.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using Newtonsoft.Json.Serialization;
+using TechStore_BE.DataConnection;
+using TechStore_BE.Models;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("QLSaling"));
+});
+
+builder.Services.Configure<Email>(builder.Configuration.GetSection("Email"));
+builder.Services.AddScoped<EmailService>(); //register EmailService in system DI
+
+builder.Services.AddSession(options =>
+{
+    options.Cookie.Name = ".WebProject.Session";
+    options.IdleTimeout = TimeSpan.FromHours(3);
+});
+
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.CheckConsentNeeded = context => true;
+    options.MinimumSameSitePolicy = SameSiteMode.None;
+});
+
+// Add JSON Serializer settings
+builder.Services.AddControllersWithViews()
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+        options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+    });
+
+var app = builder.Build();
+
+// Thêm đoạn này TRƯỚC UseRouting
+app.UseCors("AllowOrigin"); // ✅ Bật CORS sớm
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+
+app.UseRouting(); 
+app.UseSession();
+app.UseAuthorization();
+
+app.MapControllers(); 
+
+app.Run();
---
+++ TechStore_BE/Services/EmailService.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Mail;
+using System.Net;
+
+public class EmailService
+{
+    private readonly IConfiguration _config;
+
+    public EmailService(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public async Task SendEmailAsync(string toEmail, string subject, string body)
+    {
+        var fromEmail = _config["Email:From"];
+        var password = _config["Email:AppPassword"];
+
+        Console.WriteLine( "Đang gửi email đến: " + toEmail);
+        Console.WriteLine("From: " + fromEmail);
+        Console.WriteLine("Subject: " + subject);
+        Console.WriteLine("Body preview: " + body.Substring(0, Math.Min(100, body.Length)));
+
+        var smtpClient = new SmtpClient("smtp.gmail.com")
+        {
+            Port = 587,
+            Credentials = new NetworkCredential(fromEmail, password), //verify
+            EnableSsl = true,
+        };
+
+        var mailMessage = new MailMessage
+        {
+            From = new MailAddress(fromEmail),
+            Subject = subject,
+            Body = body,
+            IsBodyHtml = true,
+        };
+        mailMessage.To.Add(toEmail);
+
+        try
+        {
+            await smtpClient.SendMailAsync(mailMessage);
+            Console.WriteLine("Gửi email thành công.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Gửi email thất bại: " + ex.Message);
+            throw;
+        }
+    }
+
+}
